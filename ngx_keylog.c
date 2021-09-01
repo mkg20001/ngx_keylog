aaba9623a027f9e0cf8d222cbf1aeb2f2dc3fbdb
Maciej Krüger <mkg20001@gmail.com>
2021-09-01 19:42:54 +0200

    feat: cleanup

--- ngx_keylog.c
+++ ngx_keylog.c
@@ -46,11 +46,8 @@ static void keylog_callback(const SSL *ssl, const char *line)
 static ngx_int_t
 ngx_http_keylog_access_handler(ngx_http_request_t *r)
 {
-    /* ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "http hello_world handler"); */
-
-    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
-                  "keylog access attached");
+    ngx_log_debug(NGX_LOG_NOTICE, r->connection->log, 0,
+                  "keylog attached to connection %d", r->connection->number);
 
     if (r->connection->ssl->session_ctx != NULL)
         SSL_CTX_set_keylog_callback(r->connection->ssl->session_ctx, keylog_callback);
@@ -58,29 +55,18 @@ ngx_http_keylog_access_handler(ngx_http_request_t *r)
     return NGX_OK;
 }
 
-/* SSL *SSL_new(SSL_CTX *ctx)
-{
-    static SSL *(*func)();
-    if (!func) {
-        func = ngx_dlsym(RTLD_NEXT, "SSL_new");
-    }
-    * Override any previous key log callback. *
-    SSL_CTX_set_keylog_callback(ctx, keylog_callback);
-    return func(ctx);
-} */
-
 static ngx_int_t
 ngx_http_keylog_init(ngx_conf_t *cf)
 {
     ngx_http_handler_pt        *h;
     ngx_http_core_main_conf_t  *cmcf;
 
-    ngx_log_error(NGX_LOG_EMERG, cf->log, 0,
+    ngx_log_debug(NGX_LOG_NOTICE, cf->log, 0,
                   "init keylog");
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
-    h = ngx_array_push(&cmcf->phases[NGX_HTTP_PRECONTENT_PHASE].handlers);
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_PREACCESS_PHASE].handlers);
     if (h == NULL) {
         return NGX_ERROR;
     }
@@ -159,10 +145,6 @@ ngx_http_keylog_init_main_conf(ngx_conf_t *cf, void *conf)
     }
 
     keylog_file_fd = klcf->keylog_fd;
-    /* ngx_connection_t *c = NULL;
-    if (c->ssl->session_ctx != NULL)
-        SSL_CTX_set_keylog_callback(c->ssl->session_ctx, keylog_callback); */
-
 
     return NGX_CONF_OK;
 }
