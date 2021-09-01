b02a1be30322e6fc16dc98a3cfaceaab855fe839
Maciej Krüger <mkg20001@gmail.com>
2021-09-01 19:20:15 +0200

    feat: make it work

--- ngx_keylog.c
+++ ngx_keylog.c
@@ -14,6 +14,8 @@ typedef struct {
     ngx_str_t current_keylog_location;
 } ngx_http_keylog_main_conf_t;
 
+static int keylog_file_fd = -1;
+
 static ngx_command_t ngx_http_keylog_commands[] = {
         { ngx_string("keylog_location"),
           NGX_HTTP_MAIN_CONF|NGX_CONF_1MORE,
@@ -29,6 +31,64 @@ static ngx_command_t ngx_http_keylog_commands[] = {
           NULL
         }
 };
+
+/* Key extraction via the new OpenSSL 1.1.1 API. */
+static void keylog_callback(const SSL *ssl, const char *line)
+{
+    if (keylog_file_fd >= 0) {
+        if (write(keylog_file_fd, line, strlen(line))) {
+        }
+        if (write(keylog_file_fd, "\n", 1)) {
+        }
+    }
+}
+
+static ngx_int_t
+ngx_http_keylog_access_handler(ngx_http_request_t *r)
+{
+    /* ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+                   "http hello_world handler"); */
+
+    ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0,
+                  "keylog access attached");
+
+    if (r->connection->ssl->session_ctx != NULL)
+        SSL_CTX_set_keylog_callback(r->connection->ssl->session_ctx, keylog_callback);
+
+    return NGX_OK;
+}
+
+/* SSL *SSL_new(SSL_CTX *ctx)
+{
+    static SSL *(*func)();
+    if (!func) {
+        func = ngx_dlsym(RTLD_NEXT, "SSL_new");
+    }
+    * Override any previous key log callback. *
+    SSL_CTX_set_keylog_callback(ctx, keylog_callback);
+    return func(ctx);
+} */
+
+static ngx_int_t
+ngx_http_keylog_init(ngx_conf_t *cf)
+{
+    ngx_http_handler_pt        *h;
+    ngx_http_core_main_conf_t  *cmcf;
+
+    ngx_log_error(NGX_LOG_EMERG, cf->log, 0,
+                  "init keylog");
+
+    cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
+
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_PRECONTENT_PHASE].handlers);
+    if (h == NULL) {
+        return NGX_ERROR;
+    }
+
+    *h = ngx_http_keylog_access_handler;
+
+    return NGX_OK;
+}
 
 static void *
 ngx_http_keylog_create_main_conf(ngx_conf_t *cf)
@@ -84,7 +144,7 @@ ngx_http_keylog_init_main_conf(ngx_conf_t *cf, void *conf)
         }
 
         ngx_log_error(NGX_LOG_NOTICE, cf->log, 0,
-                      "keylogging enabled, logging to %*s", klcf->keylog_location.len, klcf->keylog_location.data);
+                      "keylog enabled, logging to %*s", klcf->keylog_location.len, klcf->keylog_location.data);
     } else {
         // cleanup fd and current loc
         if (klcf->keylog_fd >= 0) {
@@ -98,12 +158,18 @@ ngx_http_keylog_init_main_conf(ngx_conf_t *cf, void *conf)
         }
     }
 
+    keylog_file_fd = klcf->keylog_fd;
+    /* ngx_connection_t *c = NULL;
+    if (c->ssl->session_ctx != NULL)
+        SSL_CTX_set_keylog_callback(c->ssl->session_ctx, keylog_callback); */
+
+
     return NGX_CONF_OK;
 }
 
 static ngx_http_module_t  ngx_http_keylog_ctx = {
         NULL,                          /* preconfiguration */
-        NULL,                          /* postconfiguration */
+        ngx_http_keylog_init,                          /* postconfiguration */
 
         ngx_http_keylog_create_main_conf,                          /* create main configuration */
         ngx_http_keylog_init_main_conf,                          /* init main configuration */
