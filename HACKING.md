c0cc625e1efe4df9570175c070883940c715f451
Maciej Krüger <mkg20001@gmail.com>
2021-09-01 18:10:06 +0200

    feat: work on keylog module

--- HACKING.md
+++ HACKING.md
@@ -1,11 +1,16 @@
 # hacking
 
 ```
+# generate cert
+nix-env -p mkcert --run "mkcert localhost"
+# enter shell with nginx
 nix-shell "<nixpkgs>" -A nginx
+# run those in that shell
 unpackPhase
 cd nginx-*
 configureFlags="$configureFlags --add-module=$PWD/../"
 configurePhase
 # then for testing
 buildPhase
+./objs/nginx -c $PWD/../test.conf
 ```
--- ngx_keylog.c
+++ ngx_keylog.c
@@ -7,7 +7,7 @@ typedef struct {
     ngx_flag_t keylog_enabled;
 } ngx_http_keylog_main_conf_t;
 
-static ngx_command_t ngx_http_keylog_commands = {
+static ngx_command_t ngx_http_keylog_commands[] = {
         { ngx_string("keylog_location"),
           NGX_HTTP_MAIN_CONF|NGX_CONF_1MORE,
           ngx_conf_set_str_slot,
@@ -18,43 +18,52 @@ static ngx_command_t ngx_http_keylog_commands = {
           NGX_HTTP_MAIN_CONF|NGX_CONF_FLAG,
           ngx_conf_set_flag_slot,
           NGX_HTTP_MAIN_CONF_OFFSET,
-          offsetof(ngx_http_keylog_main_conf_t, keylog_enabled)
+          offsetof(ngx_http_keylog_main_conf_t, keylog_enabled),
+          NULL
         }
 };
 
 static void *
-ngx_http_keylog_create_main_conf(ngx_conf_t *cf) {
+ngx_http_keylog_create_main_conf(ngx_conf_t *cf)
+{
     ngx_http_keylog_main_conf_t  *conf;
     conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_keylog_main_conf_t));
     if (conf == NULL) {
         return NGX_CONF_ERROR;
     }
-    // conf->keylog_location = NGX_CONF_UNSET;
-    // conf->keylog_enabled = NGX_CONF_UNSET;
+
+    conf->keylog_enabled = NGX_CONF_UNSET;
+
+    return conf;
 }
 
+
 static char *
-ngx_http_keylog_merge_main_conf(ngx_conf_t *cf, void *parent, void *child) {
-    ngx_http_keylog_main_conf_t *prev = parent;
-    ngx_http_keylog_main_conf_t *conf = child;
+ngx_http_keylog_init_main_conf(ngx_conf_t *cf, void *conf)
+{
+    ngx_http_keylog_main_conf_t *klcf;
+    klcf = conf;
 
-    ngx_conf_merge_str_value(conf->keylog_location, prev->keylog_location, 10);
-    ngx_conf_merge_value(conf->keylog_enabled, prev->keylog_enabled, 20);
+    if (klcf->keylog_enabled == 1) {
+        if (!klcf->keylog_location.len) {
+            ngx_log_error(NGX_LOG_EMERG, cf->log, 0,
+                          "keylog enabled but no keylog_location set");
+            return NGX_CONF_ERROR;
+        }
 
-    /* if (conf->keylog_enabled != NULL && ngx_strlen(conf->keylog_location) > 0) {
-        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
-                           "keylog enabled but keylog_location not set");
-        return NGX_CONF_ERROR;
-    } */
+        ngx_log_error(NGX_LOG_NOTICE, cf->log, 0,
+                      "keylogging enabled, logging to %s", klcf->keylog_location);
+    }
 
     return NGX_CONF_OK;
 }
-static ngx_http_module_t  ngx_http_keylog_module_ctx = {
+
+static ngx_http_module_t  ngx_http_keylog_ctx = {
         NULL,                          /* preconfiguration */
         NULL,                          /* postconfiguration */
 
         ngx_http_keylog_create_main_conf,                          /* create main configuration */
-        ngx_http_keylog_merge_main_conf,                          /* init main configuration */
+        ngx_http_keylog_init_main_conf,                          /* init main configuration */
 
         NULL,                          /* create server configuration */
         NULL,                          /* merge server configuration */
@@ -63,9 +72,9 @@ static ngx_http_module_t  ngx_http_keylog_module_ctx = {
         NULL /* merge location configuration */
 };
 
-ngx_module_t  ngx_http_keylog_module = {
+ngx_module_t  ngx_http_keylog = {
         NGX_MODULE_V1,
-        &ngx_http_keylog_module_ctx, /* module context */
+        &ngx_http_keylog_ctx, /* module context */
         ngx_http_keylog_commands,   /* module directives */
         NGX_HTTP_MODULE,               /* module type */
         NULL,                          /* init master */
